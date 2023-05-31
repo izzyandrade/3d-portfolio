3aa23ae9563cbd318823a0cba534b0ddc3f61889
izzyandrade <israelcarneiro97@gmail.com>
2023-05-31 19:08:55 -0300

    feat: added scroll button to hero component

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,5 +1,6 @@
 import { styles } from '../styles';
 import ComputersCanvas from './canvas/Computers';
+import { motion } from 'framer-motion';
 
 const Hero = () => {
   return (
@@ -23,6 +24,23 @@ const Hero = () => {
       </div>
 
       <ComputersCanvas />
+      <div className="absolute bottom-32 flex w-full items-center justify-center xs:bottom-10">
+        <a href="#about">
+          <div className="flex h-[64px] w-[35px] items-start justify-center rounded-3xl border-4 border-secondary p-2">
+            <motion.div
+              animate={{
+                y: [0, 24, 0]
+              }}
+              transition={{
+                duration: 1.5,
+                repeat: Infinity,
+                repeatType: 'loop'
+              }}
+              className="mb-1 h-3 w-3 rounded-full bg-secondary"
+            />
+          </div>
+        </a>
+      </div>
     </section>
   );
 };
